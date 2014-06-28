ebd92649b1a8643ed362cdb36159189174cf7c9c
markscoin <mortega@utexas.edu>
2014-06-28 11:33:32 -0500

    Chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App" }
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,7 +9,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   describe "Help Page" do
@@ -18,7 +19,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
       end
   end
   describe "About Page" do
@@ -28,7 +29,17 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
-      end
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  describe "Contact Us Page" do
+    it "Should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the title 'Ruby on Rails Tutorial Sample App | Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
   end
 end
